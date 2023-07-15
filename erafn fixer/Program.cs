54754bcf5f0a0dc4f9da1de38fce34f07a003516
Svvayyz <76277320+Svvayyz@users.noreply.github.com>
2023-07-15 14:13:49 +0200

    fixor
    fixfix

---
+++ erafn fixer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace erafn_fixer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Environment.SetEnvironmentVariable("OPENSSL_ia32cap", "~0x20000000");
+            Console.WriteLine("Enviorment variable OPENSSL_ia32cap has been set to ~0x20000000. Please note that you have to restart PC in order to update the Enviorment Variables. Press any key to exit.");
+
+            Console.ReadKey();
+        }
+    }
+}
